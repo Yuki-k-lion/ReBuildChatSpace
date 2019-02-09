bb3a962ef279cdfa02b86f6928580185c0f24dae
yuki komatsu <yuki.k.lion@gmail.com>
2019-02-09 18:01:09 +0900

    add: User profile

--- ChatSpace/config/routes.rb
+++ ChatSpace/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: :show
+  # devise_for :users
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+          registrations: 'users/registrations'
+  }
+
 end
