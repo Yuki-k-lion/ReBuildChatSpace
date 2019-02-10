d51c4dfb1acda2657d4cde7d72bdc88426a636d4
yuki komatsu <yuki.k.lion@gmail.com>
2019-02-10 12:46:17 +0900

    bug: viewのレンダリングも効かない

--- ChatSpace/app/controllers/users/registrations_controller.rb
+++ ChatSpace/app/controllers/users/registrations_controller.rb
@@ -16,12 +16,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/edit
   # def edit
-  #   super
+  #   render 'devise/registrations/edit'
   # end
 
   # PUT /resource
   # def update
-  #   super
+  #
   # end
 
   # DELETE /resource
@@ -51,17 +51,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  # アカウント編集後、プロフィール画面に移動する
+  def after_sign_up_path_for(resource)
+    user_path(id: current_user.id)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  
-  # アカウント編集後、プロフィール画面に移動する
-  def after_update_path_for(resource)
-    user_path(id: current_user.id)
-  end
+
 end
