b86c39df9fb1fc5fb716e235de437f8dcaf39ecf
yuki komatsu <yuki.k.lion@gmail.com>
2019-02-10 15:55:16 +0900

    fix: バグの原因であるルーティングの順番を変更

--- ChatSpace/config/routes.rb
+++ ChatSpace/config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
-  resources :users, only: :show
+
   # devise_for :users
   # ログイン、アカウント編集後、任意のページに推移させるための記述
   devise_for :users, controllers: {
           registrations: 'users/registrations'
   }
+  resources :users, only: :show
 
 end
